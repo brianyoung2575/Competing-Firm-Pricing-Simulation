b88a4057cbd62dc644c2179c325fdb955fbe30b9
Rezgen <joe@robertsfamily.name>
2026-04-28 18:01:11 +1200

    Update simple.py

--- strategies/simple.py
+++ strategies/simple.py
@@ -1,8 +1,8 @@
-from .base import BaseStrategy
+from strategies.base import BaseStrategy
 
 class SimpleStrategy(BaseStrategy):
     def __init__(self, price=10):
-        self.price = price
+        self.price = float(price)
 
     def choose_price(self, state):
         return self.price
